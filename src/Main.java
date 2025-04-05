f259099028fb652003c55fd2677e5dadaf01637a
ZYTON <priyanshu7pandey@gmail.com>
2025-04-05 23:41:21 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        // TEMPERATURE CONVERTER PROGRAM
+        Scanner scanner = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.println("Temperature converter program ");
+
+        System.out.print("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit (C or F): ");
+        unit = scanner.next().toUpperCase();
+
+        // (condition) ? true : false;
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5/9 : (temp * 9/5) + 32;
+
+        System.out.printf("%.1f°%s" , newTemp, unit);
+
+        scanner.close();
+    }
+}
